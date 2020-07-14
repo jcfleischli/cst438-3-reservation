e79c0c6e87e809e061ad68f27f41d8e41ca29b17
John Fleischli <johnf@loanservicingsoft.com>
2020-07-13 18:51:41 -0700

    sets up servers for message queue

---
+++ cst438-3-reservation-1/src/main/java/cst4383reservation/Cst4383Reservation1Application.java
@@ -0,0 +1,13 @@
+package cst4383reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst4383Reservation1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst4383Reservation1Application.class, args);
+	}
+
+}
